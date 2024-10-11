0632feda206b42f8028b5e8a339084670a824567
Dylan Cuskelly <dylancusk@gmail.com>
2024-10-11 13:14:13 +0100

    this one

--- raspberry_button.py
+++ raspberry_button.py
@@ -5,7 +5,6 @@ from PIL import Image, ImageTk
 import datetime
 import csv
 import RPi.GPIO as GPIO
-import threading
 
 class ImageDisplayApp:
     def __init__(self, root):
@@ -19,26 +18,12 @@ class ImageDisplayApp:
 
         self.select_folder_button = tk.Button(root, text="Select Folder", command=self.select_folder)
         self.select_folder_button.pack()
-        
-        # Remove the "Record Image" button
-        # self.save_button = tk.Button(root, text="Record Image", command=self.record_image, state=tk.DISABLED)
-        # self.save_button.pack()
 
         # Setup GPIO for physical button
-        self.gpio_button_pin = 10  # GPIO pin number
+        self.gpio_button_pin = 17  # GPIO pin number
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.gpio_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.add_event_detect(self.gpio_button_pin, GPIO.RISING, callback=self.gpio_button_pressed, bouncetime=300)
-
-        # Start a thread to handle GPIO events
-        self.gpio_thread = threading.Thread(target=self.gpio_event_loop)
-        self.gpio_thread.daemon = True
-        self.gpio_thread.start()
-
-    def gpio_event_loop(self):
-        while True:
-            if GPIO.event_detected(self.gpio_button_pin):
-                self.gpio_button_pressed(self.gpio_button_pin)
+        GPIO.setup(self.gpio_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(self.gpio_button_pin, GPIO.FALLING, callback=self.gpio_button_pressed, bouncetime=300)
 
     def select_folder(self):
         self.image_folder = filedialog.askdirectory()
@@ -51,9 +36,14 @@ class ImageDisplayApp:
         if self.image_files:
             image_path = os.path.join(self.image_folder, self.image_files[self.image_index])
             image = Image.open(image_path)
+            # Resize image if necessary to fit the screen
+            image = image.resize((320, 240), Image.ANTIALIAS)  # Adjust size as needed
             photo = ImageTk.PhotoImage(image)
             self.display_label.config(image=photo)
-            self.display_label.image = photo
+            self.display_label.image = photo  # Keep a reference to avoid garbage collection
+            self.current_image_name = self.image_files[self.image_index]
+            self.image_index = (self.image_index + 1) % len(self.image_files)
+            self.root.after(5000, self.next_image)
 
     def record_image(self):
         if self.image_files:
@@ -67,7 +57,7 @@ class ImageDisplayApp:
             self.next_image()
 
     def gpio_button_pressed(self, channel):
-        self.record_image()
+        self.root.after(0, self.record_image)
 
 if __name__ == "__main__":
     root = tk.Tk()
